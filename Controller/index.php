a5cbb4b5e550c60e170aadb50bf7201fb3711fe5
ItzDrac <149488364+ItzDrac@users.noreply.github.com>
2026-05-02 15:31:24 +0200

    Añado auth

--- Controller/index.php
+++ Controller/index.php
@@ -1,7 +1,8 @@
 <?php
+require_once 'auth.php';
 require_once '../Model/Vehiculo.php';
 
 // Obtenemos todos los coches en oferta para mostrarlos en la vista
 $coches = Vehiculo::getOfertas();
 
-include '../View/index/index_view.php';
+include '../View/index/index_view.php';
